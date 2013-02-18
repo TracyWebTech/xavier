2f2c444f11baf48163ec69356c2c5e4e462bdcb8
Luan Pablo <luanpab@gmail.com>
2013-02-18 15:29:57 -0300

    Adding school app

--- school/settings.py
+++ school/settings.py
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'school',
 )
 
 # A sample logging configuration. The only tangible logging
