0072bf6fca12df1e8c0cf5f09672485b2d3a112f
Luan Pablo <luanpab@gmail.com>
2013-02-18 16:17:29 -0300

    adding missing file from last commit

---
+++ school/local_settings_example.py
@@ -0,0 +1,16 @@
+from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dbschool.db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '123'
