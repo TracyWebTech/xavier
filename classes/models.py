9cfd90a7b2f1d0e80cbd7d83d99cd88dd5592398
Luan Pablo <luanpab@gmail.com>
2013-02-25 11:29:27 -0300

    Adding django translation in models

--- classes/models.py
+++ classes/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from period.models import Period
 from accounts.models import Student, Teacher
 from subject.models import Subject
 
 
 class Grade(models.Model):
-    name = models.CharField(max_length=50)
-    grade_type = models.CharField(max_length=50)
+    name = models.CharField(_('name'), max_length=50)
+    grade_type = models.CharField(_('grade type'), max_length=50)
 
 
 class Class(models.Model):
-    identification = models.CharField(max_length=20)
+    identification = models.CharField(_('identification'), max_length=20)
     period = models.ForeignKey(Period)
     students = models.ManyToManyField(Student)
     grade = models.ForeignKey(Grade)
