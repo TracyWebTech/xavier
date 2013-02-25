5e9d4799265284c7a325f7059f162af9ff6ec043
Luan Pablo <luanpab@gmail.com>
2013-02-25 14:43:37 -0300

    Adding fixtures to school

---
+++ school/fixtures/initial_data.py
@@ -0,0 +1,42 @@
+[
+{
+    "pk": 1,
+    "model": "auth.group",
+    "fields": {
+        "name": "Student",
+        "permissions": []
+    }
+},
+{
+    "pk": 2,
+    "model": "auth.group",
+    "fields": {
+        "name": "Teacher",
+        "permissions": []
+    }
+},
+{
+    "pk": 3,
+    "model": "auth.group",
+    "fields": {
+        "name": "Employee",
+        "permissions": []
+    }
+},
+{
+    "pk": 4,
+    "model": "auth.group",
+    "fields": {
+        "name": "Director",
+        "permissions": []
+    }
+},
+{
+    "pk": 5,
+    "model": "auth.group",
+    "fields": {
+        "name": "Secretary",
+        "permissions": []
+    }
+}
+]
