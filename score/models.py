a27bac4c6785dd48ec8dfd6fc354eda867361654
Luan Pablo <luanpab@gmail.com>
2013-02-25 14:43:16 -0300

    Adding student to score app

--- score/models.py
+++ score/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from accounts.models import Student
 from classes.models import ClassSubject
 from period.models import SubPeriod
 
@@ -9,9 +10,17 @@ class EvaluationCriteria(models.Model):
     weight = models.SmallIntegerField(_('weight'))
     class_subject = models.ForeignKey(ClassSubject)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Score(models.Model):
+    student = models.ForeignKey(Student)
     score = models.IntegerField(_('score'))
     criteria = models.ForeignKey(EvaluationCriteria)
     class_subject = models.ForeignKey(ClassSubject)
     subperiod = models.ForeignKey(SubPeriod)
+
+
+    def __unicode__(self):
+        return self.score
