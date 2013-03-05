5a2cbe2dcfafcbbd65df4dd9fa3bf74f75f3bcbc
Luan Pablo <luanpab@gmail.com>
2013-03-05 10:07:40 -0300

    adding tests for subjects

--- subjects/tests.py
+++ subjects/tests.py
@@ -1,16 +1,10 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import Subject
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    fixtures = ['subjects_for_test.json', ]
+    def test_duplicate_subject(self):
+        with self.assertRaises(ValidationError):
+            Subject.objects.create(name='Geografia')
