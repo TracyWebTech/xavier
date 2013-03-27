0431c0805c4a8156a9c6361ab99b75ab1cdba2f9
Matheus Figueiredo <matheus@tracy.com.br>
2013-03-27 19:00:38 -0300

    Mapping urls for score app

--- scores/views.py
+++ scores/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+
+from django.shortcuts import render
+
+
+def list(request, school_slug, class_slug, year, period_slug):
+    context = {
+        'class': class_slug,
+        'year': year,
+        'period': period_slug,
+    }
+
+
+    return render(request, 'scores/list.html', context)
--- xavier/urls.py
+++ xavier/urls.py
@@ -11,8 +11,9 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^school/', include('schools.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'', include('schools.urls')),
 )
