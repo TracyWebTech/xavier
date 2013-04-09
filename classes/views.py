78067ceaf2481dd66d831073c083d0fc4549f066
Guilherme Gondim <semente@taurinus.org>
2013-04-09 18:07:53 -0300

    Add initial classes templates

--- classes/views.py
+++ classes/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.forms.models import modelform_factory
+
+from xavier.views import ModelView
+
+from .models import Class
+
+
+class ClassView(ModelView):
+    paginate_by = 20
+
+    def get_query_set(self, request, *args, **kwargs):
+        # Filter items only from current school
+        qs = super(ModelView, self).get_query_set(request, *args, **kwargs)
+        return qs.filter(period__school=self.get_current_school(request))
+
+
+class_views = ClassView(Class)
