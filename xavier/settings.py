2766e39166519fcee932f0dac533d573f60e00c6
Guilherme Gondim <semente@taurinus.org>
2013-04-10 18:33:42 -0300

    Add missing context processor in settings

--- xavier/settings.py
+++ xavier/settings.py
@@ -99,6 +99,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    'schools.context_processors.current_school',
 )
 
 MIDDLEWARE_CLASSES = (
