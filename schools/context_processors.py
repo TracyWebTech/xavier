41b763c3e3e3c39c364bf5c763ad4dd4a96788b3
Guilherme Gondim <semente@taurinus.org>
2013-04-10 18:45:51 -0300

    Just add a comment

--- schools/context_processors.py
+++ schools/context_processors.py
@@ -4,4 +4,6 @@ from schools.models import School
 
 
 def current_school(request):
+    # School's manager already performs a cache for the
+    # ``get_current`` method so you don't need worry about that
     return dict(current_school=School.objects.get_current(request))
