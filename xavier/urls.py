75e5ca6ae75f037ca730d5c20727c8557e6281be
Guilherme Gondim <semente@taurinus.org>
2013-04-12 12:46:31 -0300

    Add attendances urls

--- xavier/urls.py
+++ xavier/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from xavier.views import HomePageView
 
 
+# TODO: Find a better place to put the lines below
+from django.template.loader import add_to_builtins
+add_to_builtins('django.templatetags.i18n')
+
+
 admin.autodiscover()
 
 
@@ -12,5 +17,6 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('accounts.urls')),
     url(r'^classes/', include('classes.urls')),
     url(r'^periods/', include('periods.urls')),
+    url(r'^attendances/', include('attendances.urls')),
     url(r'^$', HomePageView.as_view(), name='homepage'),
 )
