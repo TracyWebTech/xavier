15643e53419a699811989876c3ada084933b5317
Luan Pablo <luanpab@gmail.com>
2013-04-15 17:18:38 -0300

    Adding scores list

--- scores/urls.py
+++ scores/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('scores.views',
-    url(r'(?P<year>\d{4})/(?P<class_slug>[-\w]+)/?$',
-        'list', name='list')
+    url(r'(?P<year>\d{4})/(?P<class_subject_slug>[-\w]+)/?$',
+        'list', name='scores_list'),
+    url(r'get_score', 'get_score', name='get_score')
 )
--- scores/views.py
+++ scores/views.py
@@ -1,18 +1,26 @@
 # Create your views here.
 
+import json
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 
-from classes.models import Class
+from classes.models import ClassSubject
 from periods.models import Period
 
 
-def list(request, year, class_slug):
-    class_obj = get_object_or_404(Class, slug=class_slug)
-    students = class_obj.students.filter()
-    context = {
-        'year': year,
-        'class': class_obj,
-        'students': students,
-    }
+def list(request, year, class_subject_slug):
+    class_subject = get_object_or_404(ClassSubject, slug=class_subject_slug)
+    students = class_subject.classroom.students.filter()
 
-    return render(request, 'scores/list.html', context)
+    return render(request, 'scores/list.html', {'year': year,
+        'class': class_subject,
+        'students': students,
+        'criterias': class_subject.evaluationcriteria_set.values(),
+    })
+
+def get_score(request):
+    # scores = request.POST.get('scores', None)
+    if not scores:
+        return HttpResponseBadRequest()
+    average = 0
+    return HttpResponse(json.dumps(average), mimetype="application/json")
