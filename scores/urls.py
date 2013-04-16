89ba6209c7033ef7ad825302667643d8d181de6e
Luan Pablo <luanpab@gmail.com>
2013-04-16 14:34:28 -0300

    Adding logic and lists to scores templates

--- scores/urls.py
+++ scores/urls.py
@@ -1,8 +1,9 @@
-
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('scores.views',
-    url(r'(?P<year>\d{4})/(?P<class_subject_slug>[-\w]+)/?$',
-        'list', name='scores_list'),
-    url(r'get_score', 'get_score', name='get_score')
+    url(r'^(?P<year>\d{4})/(?P<class_slug>[-\w]+)/$',
+        'classes_list', name='classes_list'),
+    url(r'^(?P<year>\d{4})/(?P<class_slug>[-\w]+)/(?P<subject_slug>[-\w]+)/$',
+        'scores_list', name='scores_list'),
+    url(r'^get_score$', 'get_score', name='get_score')
 )
--- scores/views.py
+++ scores/views.py
@@ -3,24 +3,70 @@
 import json
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
+from django.utils import simplejson
 
-from classes.models import ClassSubject
+from accounts.models import Student
+from classes.models import Class, ClassSubject
 from periods.models import Period
+from scores.models import EvaluationCriteria, Score, StudentScore
 
 
-def list(request, year, class_subject_slug):
-    class_subject = get_object_or_404(ClassSubject, slug=class_subject_slug)
+def classes_list(request, year, class_slug):
+    class_subjects = ClassSubject.objects.filter(classroom__slug=class_slug)
+    return render(request, 'scores/classes_list.html', {
+        'title': unicode(Class.objects.get(slug=class_slug)),
+        'class_subjects': class_subjects
+    })
+
+
+def scores_list(request, year, class_slug, subject_slug):
+    class_subject = get_object_or_404(ClassSubject,
+                                      classroom__slug=class_slug,
+                                      subject__slug=subject_slug)
     students = class_subject.classroom.students.filter()
+    subperiod = class_subject.classroom.period.subperiod_set
+    subperiod = subperiod.prefetch_related('period')
 
-    return render(request, 'scores/list.html', {'year': year,
-        'class': class_subject,
+    return render(request, 'scores/scores_list.html', {'year': year,
+        'title': unicode(class_subject),
+        'class_subject': class_subject,
         'students': students,
         'criterias': class_subject.evaluationcriteria_set.values(),
     })
 
+
 def get_score(request):
-    # scores = request.POST.get('scores', None)
-    if not scores:
+    data = request.POST.get('data', None)
+    student_id = request.POST.get('student_id', None)
+    if not data or not student_id:
         return HttpResponseBadRequest()
+
+    student = Student.objects.get(pk=student_id)
+
+    scores = simplejson.loads(data)
     average = 0
+    weight = 0
+    for criteria_id, score in scores.items():
+        criteria = EvaluationCriteria.objects.get(pk=criteria_id)
+        criteria_weight = float(criteria.weight)
+        if score:
+            try:
+                score = float(score)
+            except score.ValueError:
+                # TODO display a message to user if a letter is send
+                msg = u'Only numbers are allowed'
+                HttpResponse(msg)
+            else:
+                # student_score = StudentScore.objects.create(score=score)
+                # Score.objects.create(
+                #     student=student,
+                #     scores=student_score,
+                #     criteria=criteria,
+                # )
+                average += score * criteria_weight
+        weight += criteria_weight
+    average = average / weight
+
+    # TODO if average is bigger than 10, set a msg and the average field to 10
+    # or don't allow values bigger than 10 on template
     return HttpResponse(json.dumps(average), mimetype="application/json")
