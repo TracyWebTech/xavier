c7503ca9bf57f424f08036ec14413dba424f55ef
Sergio Oliveira <sergio@tracy.com.br>
2013-04-17 17:39:15 -0300

    Adding flags into class listing

--- attendances/templatetags/attendances_tags.py
+++ attendances/templatetags/attendances_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils import timezone
+
+from attendances.models import AttendanceBook
 
 register = template.Library()
 
@@ -6,3 +9,16 @@ register = template.Library()
 @register.filter
 def is_late(student, attendance_book):
     return attendance_book.is_late(student)
+
+@register.filter
+def absent_students(classroom, day=None):
+    if day is None:
+        day = timezone.now()
+
+    n_students = classroom.students.count()
+    try:
+        attbook = classroom.attendancebook_set.get(day=day)
+    except AttendanceBook.DoesNotExist:
+        return n_students # all absent
+
+    return n_students - attbook.students.count()
