147f535d33655038c2342f03883fbe48f35ee074
Guilherme Gondim <semente@taurinus.org>
2013-04-17 16:26:21 -0300

    Revert "Revert "Add status and explanation fields""
    This reverts commit fed7a9f2c0d18da283d53ccd33f5b5619bff4822.

--- attendances/templatetags/attendances_tags.py
+++ attendances/templatetags/attendances_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter
 def is_late(student, attendance_book):
     return attendance_book.is_late(student)
+
+@register.filter
+def is_present(student, attendance_book):
+    return attendance_book.is_present(student)
+
+@register.filter
+def is_absent(student, attendance_book):
+    return attendance_book.is_absent(student)
