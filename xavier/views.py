f0ce8e97cf42642d52e6ff6e94f356a15671794f
Guilherme Gondim <semente@taurinus.org>
2013-04-18 16:25:05 -0300

    Add GenericTemplateMixin

--- xavier/views.py
+++ xavier/views.py
@@ -12,3 +12,22 @@ class HomePageView(generic.TemplateView):
         context = super(HomePageView, self).get_context_data(**kwargs)
         #context[''] = 
         return context
+
+
+class GenericTemplateMixin(object):
+    base_template = None
+
+    def get_context_data(self, **kwargs):
+        meta = self.model._meta
+        context = super(GenericTemplateMixin, self).get_context_data(**kwargs)
+        context.update({
+            'base_template': self.base_template,
+            'verbose_name': meta.verbose_name,
+            'verbose_name_plural': meta.verbose_name,
+        })
+        return context
+
+    def get_template_names(self):
+        names = super(GenericTemplateMixin, self).get_template_names()
+        names.append('generic/object%s.html' % self.template_name_suffix)
+        return names
