c85230516ca68e9e5d8ce74bfe9cf29c3a34a4a0
Guilherme Gondim <semente@taurinus.org>
2013-04-19 16:51:50 -0300

    Add CurrentSchoolMiddleware

--- xavier/settings.py
+++ xavier/settings.py
@@ -112,6 +112,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'schools.middleware.CurrentSchoolMiddleware',
     'xavier.middleware.LoginRequiredMiddleware',
 )
 
