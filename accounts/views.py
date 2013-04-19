ee8c6193b18803da529a71eaf1f198ddc2186fa9
Guilherme Gondim <semente@taurinus.org>
2013-04-19 17:44:28 -0300

    Simplify accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -2,53 +2,25 @@
 
 from xavier import views
 from accounts import models
-from schools.models import School
-
-
-class AccountBaseMixin(object):
-
-    def get_queryset(self):
-        queryset = super(AccountBaseMixin, self).get_queryset()
-        return queryset.filter(school=self.current_school)
-
-
-class AccountList(AccountBaseMixin, views.ListView):
-    paginate_by = 20
-
-
-class AccountDetail(AccountBaseMixin, views.DetailView):
-    pass
-
-
-class AccountCreate(AccountBaseMixin, views.CreateView):
-    pass
-
-
-class AccountUpdate(AccountBaseMixin, views.UpdateView):
-    pass
-
-
-class AccountDelete(AccountBaseMixin, views.DeleteView):
-    success_url = '../../'
 
 
 # Students
-student_list = AccountList.as_view(model=models.Student)
-student_detail = AccountDetail.as_view(model=models.Student)
-student_create = AccountCreate.as_view(model=models.Student)
-student_update = AccountUpdate.as_view(model=models.Student)
-student_delete = AccountDelete.as_view(model=models.Student)
+student_list = views.List.as_view(model=models.Student)
+student_detail = views.Detail.as_view(model=models.Student)
+student_create = views.Create.as_view(model=models.Student)
+student_update = views.Update.as_view(model=models.Student)
+student_delete = views.Delete.as_view(model=models.Student)
 
 # Employees
-employee_list = AccountList.as_view(model=models.Employee)
-employee_detail = AccountDetail.as_view(model=models.Employee)
-employee_create = AccountCreate.as_view(model=models.Employee)
-employee_update = AccountUpdate.as_view(model=models.Employee)
-employee_delete = AccountDelete.as_view(model=models.Employee)
+employee_list = views.List.as_view(model=models.Employee)
+employee_detail = views.Detail.as_view(model=models.Employee)
+employee_create = views.Create.as_view(model=models.Employee)
+employee_update = views.Update.as_view(model=models.Employee)
+employee_delete = views.Delete.as_view(model=models.Employee)
 
 # Teachers
-teacher_list = AccountList.as_view(model=models.Teacher)
-teacher_detail = AccountDetail.as_view(model=models.Teacher)
-teacher_create = AccountCreate.as_view(model=models.Teacher)
-teacher_update = AccountUpdate.as_view(model=models.Teacher)
-teacher_delete = AccountDelete.as_view(model=models.Teacher)
+teacher_list = views.List.as_view(model=models.Teacher)
+teacher_detail = views.Detail.as_view(model=models.Teacher)
+teacher_create = views.Create.as_view(model=models.Teacher)
+teacher_update = views.Update.as_view(model=models.Teacher)
+teacher_delete = views.Delete.as_view(model=models.Teacher)
