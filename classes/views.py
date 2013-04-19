7b5510923aa993eb0f1d24d0d388977de5d7bbdc
Guilherme Gondim <semente@taurinus.org>
2013-04-19 17:44:59 -0300

    Simplify classes views

--- classes/views.py
+++ classes/views.py
@@ -4,39 +4,10 @@ from django.core.urlresolvers import reverse_lazy
 
 from xavier import views
 from classes import models
-from schools.models import School
 
 
-class ClassBaseMixin(object):
-    model = models.Class
-
-    def get_queryset(self):
-        queryset = super(PeriodBaseMixin, self).get_queryset()
-        return queryset.filter(school=self.current_school)
-
-
-class ClassList(ClassBaseMixin, views.ListView):
-    paginate_by = 20
-
-
-class ClassDetail(ClassBaseMixin, views.DetailView):
-    pass
-
-
-class ClassCreate(ClassBaseMixin, views.CreateView):
-    pass
-
-
-class ClassUpdate(ClassBaseMixin, views.UpdateView):
-    pass
-
-
-class ClassDelete(views.DeleteView):
-    success_url = reverse_lazy('class-list')
-
-
-class_list = ClassList.as_view()
-class_detail = ClassDetail.as_view()
-class_create = ClassCreate.as_view()
-class_update = ClassUpdate.as_view()
-class_delete = ClassDelete.as_view()
+class_list = views.List.as_view(model=models.Class)
+class_detail = views.Detail.as_view(model=models.Class)
+class_create = views.Create.as_view(model=models.Class)
+class_update = views.Update.as_view(model=models.Class)
+class_delete = views.Delete.as_view(model=models.Class)
