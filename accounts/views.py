97ec3c43c60c028f9d9899bf493c60ad9765410e
Guilherme Gondim <semente@taurinus.org>
2013-04-19 17:56:06 -0300

    Fix generic views names

--- accounts/views.py
+++ accounts/views.py
@@ -5,22 +5,22 @@ from accounts import models
 
 
 # Students
-student_list = views.List.as_view(model=models.Student)
-student_detail = views.Detail.as_view(model=models.Student)
-student_create = views.Create.as_view(model=models.Student)
-student_update = views.Update.as_view(model=models.Student)
-student_delete = views.Delete.as_view(model=models.Student)
+student_list = views.ListView.as_view(model=models.Student)
+student_detail = views.DetailView.as_view(model=models.Student)
+student_create = views.CreateView.as_view(model=models.Student)
+student_update = views.UpdateView.as_view(model=models.Student)
+student_delete = views.DeleteView.as_view(model=models.Student)
 
 # Employees
-employee_list = views.List.as_view(model=models.Employee)
-employee_detail = views.Detail.as_view(model=models.Employee)
-employee_create = views.Create.as_view(model=models.Employee)
-employee_update = views.Update.as_view(model=models.Employee)
-employee_delete = views.Delete.as_view(model=models.Employee)
+employee_list = views.ListView.as_view(model=models.Employee)
+employee_detail = views.DetailView.as_view(model=models.Employee)
+employee_create = views.CreateView.as_view(model=models.Employee)
+employee_update = views.UpdateView.as_view(model=models.Employee)
+employee_delete = views.DeleteView.as_view(model=models.Employee)
 
 # Teachers
-teacher_list = views.List.as_view(model=models.Teacher)
-teacher_detail = views.Detail.as_view(model=models.Teacher)
-teacher_create = views.Create.as_view(model=models.Teacher)
-teacher_update = views.Update.as_view(model=models.Teacher)
-teacher_delete = views.Delete.as_view(model=models.Teacher)
+teacher_list = views.ListView.as_view(model=models.Teacher)
+teacher_detail = views.DetailView.as_view(model=models.Teacher)
+teacher_create = views.CreateView.as_view(model=models.Teacher)
+teacher_update = views.UpdateView.as_view(model=models.Teacher)
+teacher_delete = views.DeleteView.as_view(model=models.Teacher)
--- classes/views.py
+++ classes/views.py
@@ -6,8 +6,8 @@ from xavier import views
 from classes import models
 
 
-class_list = views.List.as_view(model=models.Class)
-class_detail = views.Detail.as_view(model=models.Class)
-class_create = views.Create.as_view(model=models.Class)
-class_update = views.Update.as_view(model=models.Class)
-class_delete = views.Delete.as_view(model=models.Class)
+class_list = views.ListView.as_view(model=models.Class)
+class_detail = views.DetailView.as_view(model=models.Class)
+class_create = views.CreateView.as_view(model=models.Class)
+class_update = views.UpdateView.as_view(model=models.Class)
+class_delete = views.DeleteView.as_view(model=models.Class)
