e3f9b6dfb3b7be0d9a9f9baafb2a1a1b9c03ace0
Guilherme Gondim <semente@taurinus.org>
2013-04-19 14:04:45 -0300

    Simplify class views and urls

--- classes/urls.py
+++ classes/urls.py
@@ -4,10 +4,9 @@ from classes import views
 
 
 urlpatterns = patterns('',
-    # Class model
-    url(r'^class/$', views.ClassList.as_view(), name='class-list'),
-    url(r'^class/add/$', views.ClassCreate.as_view(), name='class-create'),
-    url(r'^class/(?P<pk>\d+)/$', views.ClassDetail.as_view(), name='class-detail'),
-    url(r'^class/(?P<pk>\d+)/edit/$', views.ClassUpdate.as_view(), name='class-update'),
-    url(r'^class/(?P<pk>\d+)/delete/$', views.ClassDelete.as_view(), name='class-delete'),
+    url(r'^class/$', views.class_list, name='class-list'),
+    url(r'^class/add/$', views.class_create, name='class-create'),
+    url(r'^class/(?P<pk>\d+)/$', views.class_detail, name='class-detail'),
+    url(r'^class/(?P<pk>\d+)/edit/$', views.class_update, name='class-update'),
+    url(r'^class/(?P<pk>\d+)/delete/$', views.class_delete, name='class-delete'),
 )
--- classes/views.py
+++ classes/views.py
@@ -3,26 +3,44 @@
 from django.core.urlresolvers import reverse_lazy
 
 from xavier import views
-from classes import models, forms
+from classes import models
+from schools.models import School
 
 
-class ClassList(views.ListView):
+class ClassBaseMixin(object):
     model = models.Class
+
+    @property
+    def current_school(self):
+        return School.objects.get_current(self.request)
+
+    def get_queryset(self):
+        queryset = super(PeriodBaseMixin, self).get_queryset()
+        return queryset.filter(school=self.current_school)
+
+
+class ClassList(ClassBaseMixin, views.ListView):
     paginate_by = 20
 
 
-class ClassDetail(views.DetailView):
-    model = models.Class
+class ClassDetail(ClassBaseMixin, views.DetailView):
+    pass
 
 
-class ClassCreate(views.CreateView):
-    model = models.Class
+class ClassCreate(ClassBaseMixin, views.CreateView):
+    pass
 
 
-class ClassUpdate(views.UpdateView):
-    model = models.Class
+class ClassUpdate(ClassBaseMixin, views.UpdateView):
+    pass
 
 
 class ClassDelete(views.DeleteView):
-    model = models.Class
     success_url = reverse_lazy('class-list')
+
+
+class_list = ClassList.as_view()
+class_detail = ClassDetail.as_view()
+class_create = ClassCreate.as_view()
+class_update = ClassUpdate.as_view()
+class_delete = ClassDelete.as_view()
