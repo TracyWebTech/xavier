3f61e8c099c14c7f3f6822cea1d9bd3514978422
Luan Pablo <luanpab@gmail.com>
2013-04-20 18:49:29 -0300

    Adding fix to caret

--- xavier/static/js/dropdown_subjects.js
+++ xavier/static/js/dropdown_subjects.js
@@ -1,4 +1,10 @@
 $(function() {
+    $('a[name="subjects"]').each(function() {
+        $ul = $(this).parent().find('ul.list_subjects');
+        if ($('li', $ul).length == 1) {
+            $(this).find('.caret').remove();
+        }
+    });
     $('a[name="subjects"]').click(function() {
         $ul = $(this).parent().find('ul.list_subjects');
         $('ul.list_subjects', $(this).parent().siblings()).hide();
