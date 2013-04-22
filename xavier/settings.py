d86a26933d7ca627fe5d0dc59ef9dea47475891d
Guilherme Gondim <semente@taurinus.org>
2013-04-22 12:36:03 -0300

    Remove tastypie from project

--- xavier/settings.py
+++ xavier/settings.py
@@ -143,7 +143,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'autofixture',
     'south',
-    'tastypie',
     'schools',
     'periods',
     'subjects',
--- xavier/urls.py
+++ xavier/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from xavier.api import v1_api
 from xavier.views import HomePageView
 
 
@@ -14,7 +13,6 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^classes/', include('classes.urls')),
