00ff912fd2d3a23d306431f752f87e845879aac4
Guilherme Gondim <semente@taurinus.org>
2013-04-23 16:38:18 -0300

    Improve attendances views, breadcrumbs and URLs

--- attendances/views.py
+++ attendances/views.py
@@ -3,8 +3,8 @@
 from datetime import date
 
 from django import http
-from django.core.exceptions import ObjectDoesNotExist
-from django.views.generic import View
+from django.core import exceptions
+from django.views import generic
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext
@@ -28,38 +28,33 @@ def get_attendance_book(classroom, day):
     return attendance_book
 
 
-class AttendanceBookList(views.ListView):
-    base_template = 'attendances/base.html'
-    model = models.AttendanceBook
-
-    def get_classroom_list(self):
-        return Class.on_school.all()
+class Index(generic.TemplateView):
+    template_name = 'attendances/index.html'
 
     def get_context_data(self, **kwargs):
-        context = super(AttendanceBookList, self).get_context_data(**kwargs)
+        context = super(Index, self).get_context_data(**kwargs)
         context.update({
-            'classroom_list': self.get_classroom_list(),
+            'title': ugettext('Attendances'),
+            'classroom_list': Class.on_school.all()
         })
         return context
 
 
-class TakeAttendance(View):
+class ClassAttendances(generic.TemplateView):
+    template_name = 'attendances/class.html'
 
-    def get(self, request, classroom):
-        classroom = get_object_or_404(Class, pk=classroom)
-        day = request.GET.get('day', date.today())
+    def get_context_data(self, **kwargs):
+        context = super(ClassAttendances, self).get_context_data(**kwargs)
+        classroom = get_object_or_404(Class, pk=kwargs['classroom'])
+        day = self.request.GET.get('day', date.today())
         attendance_book = get_attendance_book(classroom, day)
-        context = {
+        context.update({
             'title': ugettext('Attendances'),
             'subtitle': unicode(classroom),
             'classroom': classroom,
             'attendance_book': attendance_book
-        }
-        return render_to_response(
-            template_name='attendances/take_attendance.html',
-            dictionary=context,
-            context_instance=RequestContext(request)
-        )
+        })
+        return context
 
 
 def ajax_attendance_change_status(request, classroom, student):
@@ -71,7 +66,7 @@ def ajax_attendance_change_status(request, classroom, student):
             attendance_book = get_attendance_book(classroom, day)
         except ValueError:
             return http.HttpResponse(status=400)
-        except ObjectDoesNotExist:
+        except exceptions.ObjectDoesNotExist:
             return http.HttpResponse(status=400)
         attendance, created = models.Attendance.objects.get_or_create(
             attendance_book=attendance_book,
@@ -94,7 +89,7 @@ def ajax_attendance_set_explanation(request, classroom, student):
             attendance_book = get_attendance_book(classroom, day)
         except ValueError:
             return http.HttpResponse(status=400)
-        except ObjectDoesNotExist:
+        except exceptions.ObjectDoesNotExist:
             return http.HttpResponse(status=400)
         attendance, created = models.Attendance.objects.get_or_create(
             attendance_book=attendance_book,
@@ -106,12 +101,3 @@ def ajax_attendance_set_explanation(request, classroom, student):
             attendance.save()
         return http.HttpResponse(status=200)
     return http.HttpResponse(status=400)
-
-
-attendancebook_list = AttendanceBookList.as_view()
-attendancebook_detail = views.DetailView.as_view(model=models.AttendanceBook)
-attendancebook_create = views.CreateView.as_view(model=models.AttendanceBook)
-attendancebook_update = views.UpdateView.as_view(model=models.AttendanceBook)
-attendancebook_delete = views.DeleteView.as_view(model=models.AttendanceBook)
-
-takeattendance = TakeAttendance.as_view()
