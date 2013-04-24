8b301286b959672a2df6373e076764b4aba5dc1f
Guilherme Gondim <semente@taurinus.org>
2013-04-24 16:40:12 -0300

    Add template tag to verify if some day is break or school day

--- calendars/templatetags/calendars_tags.py
+++ calendars/templatetags/calendars_tags.py
@@ -1,8 +1,12 @@
 import calendar
 
+from datetime import date
+
 from django import template
-from django.utils import timezone
 from django.utils.translation import ugettext
+
+from calendars import models
+
 
 register = template.Library()
 
@@ -12,3 +16,13 @@ def get_month_name(bimester, month):
     width = 2
     month_name = calendar.month_name[bimester * width + month]
     return ugettext(month_name)
+
+
+@register.filter
+def is_break(day):
+    calendar = models.Calendar.objects.all()[0]  # TODO
+    try:
+        models.Break.objects.get(calendar=calendar, day=day)
+        return True
+    except models.Break.DoesNotExist:
+        return False
