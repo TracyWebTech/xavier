d8d36998c632656dc2f4e15a8ec00a1dd8a7f45c
Luan Pablo <luanpab@gmail.com>
2013-04-25 08:41:04 -0300

    Adding modeling changes for timetable

--- timetables/admin.py
+++ timetables/admin.py
@@ -2,11 +2,15 @@
 
 from django.contrib import admin
 
-from .models import Timetable
+from .models import Timetable, Time
 
 
 class TimetableAdmin(admin.ModelAdmin):
     pass
 
+class TimeAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Timetable, TimetableAdmin)
+admin.site.register(Time, TimeAdmin)
--- timetables/models.py
+++ timetables/models.py
@@ -2,48 +2,55 @@
 
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
+from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
+
+from schools.models import School
+from subjects.models import Subject
 
 
 class Timetable(models.Model):
-    WEEKDAY_CHOICES = (
-        ('mon', 'Monday'),
-        ('tue', 'Tuesday'),
-        ('wed', 'Wednesday'),
-        ('thu', 'Thursday'),
-        ('fri', 'Friday'),
-        ('sat', 'Saturday'),
-        ('sun', 'Sunday'),
-    )
-
-    calendar = models.ForeignKey('calendars.Calendar')
-    class_subject = models.ForeignKey('classes.ClassSubject')
-    start = models.TimeField()
-    end = models.TimeField()
-    weekday = models.CharField(
-        _('weekday'),
-        max_length=3,
-        choices=WEEKDAY_CHOICES,
-        null=True, blank=True,
-    )
-
-    # The following fields are useful to specify extra classes and/or
-    # replacement teachers
-    day = models.DateField(
-        _('day'),
-        null=True, blank=True,
-    )
-    replacement_teacher = models.ForeignKey(
-        'accounts.Teacher',
-        null=True, blank=True,
-        verbose_name=_(u'replacement teacher'),
-    )
+    school = models.ForeignKey(School)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, unique=True)
 
     class Meta:
         verbose_name = _('timetable')
         verbose_name_plural = _('timetables')
 
+    def __unicode__(self):
+        return u'{0} ({1})'.format(self.name, self.school.short_name)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(unicode(self))
+        super(Timetable, self).save(*args, **kwargs)
+
+
+class Time(models.Model):
+    timetable = models.ForeignKey(Timetable, verbose_name=_('timetable'))
+    start = models.TimeField()
+    end = models.TimeField()
+
+    def __unicode__(self):
+        return u'{0} - {1} - {2}'.format(unicode(self.timetable),
+                                         unicode(self.start),
+                                         unicode(self.end))
+
     def clean(self):
-        if (not self.weekday and not self.day) or (self.weekday and self.day):
-            error_msg = _('You must specify either a day or a weekday')
+        if self.end <= self.start:
+            error_msg = _('The start must be before the end')
+            raise ValidationError(error_msg)
+
+        intersection = Time.objects.filter(Q(
+            start__lte=self.start, end__gte=self.start
+        ) | Q(
+            start__gte=self.start, start__lt=self.end
+        ), timetable__name=self.timetable.name)
+
+        if self.pk:
+            intersection.exclude(pk=self.pk)
+
+        if intersection.exists():
+            error_msg = _('Schedule not valid')
             raise ValidationError(error_msg)
