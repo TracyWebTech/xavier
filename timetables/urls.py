6de8b8056e6a064d9fde202c337e0a570e6e38b8
Luan Pablo <luanpab@gmail.com>
2013-04-26 09:18:23 -0300

    Adding fixes to timetables urls

--- timetables/urls.py
+++ timetables/urls.py
@@ -5,9 +5,9 @@ from timetables import views
 
 urlpatterns = patterns('',
     url(r'^$', views.SchoolTimetableList.as_view(), name='school_timetables'),
-    url(r'^edit_timetable/(?P<timetable_slug>[-\w]+)$',
+    url(r'^edit/(?P<timetable_slug>[-\w]+)$',
             views.EditTimetable.as_view(), name='edit_timetable'),
-    url(r'^add_timetable$', views.AddTimetable.as_view(),
+    url(r'^add$', views.AddTimetable.as_view(),
             name='add_timetable'),
 
     url(r'^update_times$', views.UpdateTimes.as_view(), name='update_times'),
