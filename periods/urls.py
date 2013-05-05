276b71de57c1d2f79ec2478ed5440c00aaa64342
Sergio Oliveira <sergio@tracy.com.br>
2013-05-05 16:28:28 -0300

    Using generic namespace for views in urls

--- periods/urls.py
+++ periods/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import include, patterns, url
 from periods import views
 
 
-urlpatterns = patterns('',
-    url(r'^period/$', views.period_list, name='period-list'),
-    url(r'^period/add/$', views.period_create, name='period-create'),
-    url(r'^period/(?P<pk>\d+)/$', views.period_detail, name='period-detail'),
-    url(r'^period/(?P<pk>\d+)/edit/$', views.period_update, name='period-update'),
-    url(r'^period/(?P<pk>\d+)/delete/$', views.period_delete, name='period-delete'),
+urlpatterns = patterns('periods.views',
+    url(r'^period/$', 'period_list', name='period-list'),
+    url(r'^period/add/$', 'period_create', name='period-create'),
+    url(r'^period/(?P<pk>\d+)/$', 'period_detail', name='period-detail'),
+    url(r'^period/(?P<pk>\d+)/edit/$', 'period_update', name='period-update'),
+    url(r'^period/(?P<pk>\d+)/delete/$', 'period_delete', name='period-delete'),
+    url(r'^subperiod/(?P<pk>\d+)/select/$', 'period_select', name='period-select'),
 )
