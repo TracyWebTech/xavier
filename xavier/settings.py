1c5d2dbd64606ed4e448be7b38e9f53c686f0071
Sergio Oliveira <sergio@tracy.com.br>
2013-05-06 16:46:36 -0300

    Removing autofixture from settings

--- xavier/settings.py
+++ xavier/settings.py
@@ -143,7 +143,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'autofixture',
     'south',
     'schools',
     'periods',
